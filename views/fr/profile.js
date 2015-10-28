0e71c09e007b3742ae531bb8d035f0a03da26d90
Ian Vasquez <iovg75@gmail.com>
2015-10-27 21:09:29 -0400

    fixing profile page

--- views/fr/profile.js
+++ views/fr/profile.js
@@ -3,5 +3,6 @@ module.exports =
     "profile":"Profil",
     "age":"Age: ",
     "education":"Éducation: ",
-    "music":"Style de musique: "
+    "music":"Style de musique: ",
+    "dCars":"Le(s) véhicule(s)"
 };
